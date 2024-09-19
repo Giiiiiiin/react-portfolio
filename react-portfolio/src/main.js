0f94d733e0a2d7f92867220227ebba415abdf71c
Giiiiiiin <miguel_malisa@dlsl.edu.ph>
2024-09-19 20:55:39 +0800

    version 1.05

--- react-portfolio/src/main.js
+++ react-portfolio/src/main.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import Button from 'react-bootstrap/Button';
@@ -10,17 +9,16 @@ import Profile from './profile.png';
 import Card1 from './project1.jpg';
 import Card2 from './project2.jpg';
 import Card3 from './project3.jpg';
+import Sparkle from './sparkle.jpg';
+import Firefly from './firefly.jpg';
+import Verina from './verina.jpg';
+import Yinlin from './yinlin.png';
 //
-
-function Welcome(props) {
-    return <h1>Hello {props.surname}!</h1>
-}
 
 function NavBar() {
     return (    
-    
-    <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
-        <div class="container-fluid">
+        <div class="container-fluid navigation">
+         <nav class="navbar navbar-expand navbar-dark bg-dark resize" aria-label="Second navbar example" style={{width: '70vw', height: '10vh', padding: '1rem' }}>
             <h2><a class="navbar-brand" href="#">Memories Project</a></h2>
              <div class="collapse navbar-collapse" id="navbarsExample02">
                 <ul class="navbar-nav me-auto">
@@ -28,12 +26,12 @@ function NavBar() {
                         <a class="nav-link active" aria-current="page" href="#portfolio">Portfolio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
+                        <a class="nav-link" href="#about">Contact</a>
                     </li>
                 </ul>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 
     )
 }
@@ -41,15 +39,15 @@ function NavBar() {
 function Banner() {
     return (
         <header className="App-header">
-          <div class="col-lg-6 col-md-8 mx-auto" valign="center">
-            <h1 class="fw-light" id="portfolio">Memory Project Samples</h1>
-            <h6 class="lead"><p>Memories gathered from the stories of the mind.</p></h6> 
+          <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-bold">Memory Project Samples</h1>
+            <h5 class="lead"><p>Memories gathered from the stories of the mind.</p></h5> 
          </div>
         </header>
     )
 }
 
-function ModalOne(props) {
+function ModalPop(props) {
 
     return (
         <Modal
@@ -60,17 +58,13 @@ function ModalOne(props) {
         >
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-vcenter">
-              <h1>City of Fanatical Hope</h1>
+              <h1>{props.title}</h1>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>The greed that consumes.</h4>
+            <h4>{props.sub}</h4>
             <p align="justify">
-                Hope is what gives the people power to keep moving forward. 
-                As long as the people have hope, nothing can stop them from raising their ideal kingdom.
-                But, what does it mean to have hope? People can lose hope but can they have too much of it? 
-                What does it mean for someone to have too much hope? What will happen to a city if it has too much hope? 
-                In a city built upon this single concept, will it stand as strong as the fervor they show?
+                {props.content}
             </p>
           </Modal.Body>
           <Modal.Footer>
@@ -80,7 +74,7 @@ function ModalOne(props) {
       )
 }
 
-function ModalTwo(props) {
+function ModalContact(props) {
 
     return (
         <Modal
@@ -91,19 +85,30 @@ function ModalTwo(props) {
         >
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-vcenter">
-              <h1>Clockwork and Its Broken Time</h1>
+              <h1>{props.title}</h1>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>The "time" which haunts the self.</h4>
-            <p align="justify">
-                Time for everyone moves only in a single direction. Toward the future and leaving behind the past, 
-                sealing our previous actions in the bottle of history. However... is this truly the case? 
-                Much like how an ant cannot percieve the three-dimentional plain, 
-                how can mere humans say that time cannot go backward? Or maybe even... stop.
-                The flow of time that differs from person to person. A flow of time that can devour other instances.
-                The time that can consumes life of a person.
-            </p>
+            <h4>{props.sub}</h4>
+            <div class="container-sm" style={{padding: '1rem', }}>
+                <form class="row g-3 needs-validation form" action="malisa-miguel-webdevte1.html" onsubmit="alert('Message successfully sent!')">
+                    <div class="row">
+                        <label for="validationCustom01" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="Mark Shark" required />
+                    </div>
+                    <div class="row">
+                        <label for="validationCustom02" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="validationCustom02" placeholder="mark@gmail.com" required />
+                    </div>
+                    <div class="row">
+                        <label for="" class="form-label">Message</label>
+                        <textarea class="form-control" name="" id="" rows="3" placeholder="Talk to us..." required></textarea>
+                    </div>
+                    <div class="col-12" align="center">
+                        <button class="btn btn-primary" type="submit">Send Message</button>
+                    </div>
+                </form>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="danger" onClick={props.onHide}>Close</Button>
@@ -112,37 +117,8 @@ function ModalTwo(props) {
       )
 }
 
-function ModalThree(props) {
-
-    return (
-        <Modal
-          {...props}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              <h1>The Eternity That Never Lasted</h1>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>The bliss of ignorance.</h4>
-            <p align="justify">
-                "Eternal Happiness." What a wonderful ring it has in the ears. In a world where [Eternity] is exists, 
-                the people would venture towards their dreams and fulfill their deepest wishes. 
-                What can stop the people now if time was never a concern? Frolicking and frolicking...
-                Enjoying their neverending lives... The people never realized what they had lost.
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="danger" onClick={props.onHide}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      )
-}
 /*
-function Cards(props) {
+function SingleCards(props) {
     
     return (
         <div>
@@ -164,16 +140,78 @@ function Cards(props) {
                 Launch vertically centered modal
             </Button>
             */
+
+function About() {
+    const [modalContactShow, setModalContactShow] = useState(false);
+
+    return (
+        <div className="content-about" id="about">
+            <h1 style={{margin: '5rem'}}>About Me</h1>
+            <div
+                class="row justify-content-center align-items-center g-2"
+            >
+                <div class="col">
+                    <div
+                        class="row justify-content-center align-items-center g-2"
+                    >
+                        <div class="col"><img className='rounded-circle' src={Sparkle} style={{width: '3rem', height: '3rem',}} /></div>
+                    </div>
+                    <div
+                        class="row justify-content-center align-items-center g-2"
+                    >
+                        <div class="col"><img className='rounded-circle' src={Yinlin} style={{width: '3rem', height: '3rem',}} /></div>
+                        <div class="col"><img className='rounded-circle' src={Profile} style={{width: '10rem', height: '10rem', margin: '1rem'}} /></div>
+                        <div class="col"><img className='rounded-circle' src={Firefly} style={{width: '3rem', height: '3rem',}} /></div>
+                    </div>
+                    <div
+                        class="row justify-content-center align-items-center g-2"
+                    >
+                        <div class="col"><img className='rounded-circle' src={Verina} style={{width: '3rem', height: '3rem',}} /></div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div
+                        class="row justify-content-center align-items-center g-2"
+                    >
+                        <div class="col"><h5 style={{padding: '1rem'}}>The Average Computer Science Student</h5></div>
+                    </div>
+                    <div
+                        class="row justify-content-center align-items-center g-2"
+                    >
+                        <div class="col"><h6 class="fw-bold" style={{padding: '1rem'}}>"The power to create worlds is in the palm of my hands. Yet, I am only nothing but another character to the story."</h6></div>
+                    </div>
+                    <div
+                        class="row justify-content-center align-items-center g-2"
+                    >
+                        <div class="col"><h6 class="fw-light">Unhinged Writer</h6></div>
+                        <div class="col"><h6 class="fw-light">Downbad Gacha Player</h6></div>
+                    </div>
+                    
+
+                </div>
+                <Button variant="outline-success" onClick={() => setModalContactShow(true)} style={{margin: '3rem'}}>Contact Me</Button>
+
+                <ModalContact
+                    title=      "Contact Me"
+                    sub=        "miguel_malisa@dlsl.edu.ph"
+                    show={modalContactShow}
+                    onHide={() => setModalContactShow(false)}
+                />
+            </div>
+        </div>
+    )
+}
+
 function Section(props) {
 
     return (
-        <div className="App-section">
+        <div className="App-section" id="portfolio">
             <div
                 class="row justify-content-center align-items-center g-lg-2"
                 align="center"
             >
                 
-            <Card className="cardStyle" style={{width: '30rem'}}>
+            <Card className="cardStyle" style={{width: '70%'}}>
                 <Card.Img variant="top" src={props.img} />
                 <Card.Body>
                     <Card.Title><h2>{props.title}</h2></Card.Title>
@@ -184,14 +222,6 @@ function Section(props) {
                 </Card.Body>
             </Card>
          </div>
-        </div>
-    )
-}
-
-function About() {
-    return (
-        <div className="content-about">
-            <h1>About Me</h1>
         </div>
     )
 }
@@ -237,16 +267,37 @@ const MyComponent = (props) => {
                 onShow={() => setModal3Show(true)}
             />
 
-         <ModalOne
+         <ModalPop
+                title=      "City of Fanatical Hope"
+                sub=        "The greed that consumes."
+                content=    "Hope is what gives the people power to keep moving forward. 
+                            As long as the people have hope, nothing can stop them from raising their ideal kingdom.
+                            But, what does it mean to have hope? People can lose hope but can they have too much of it? 
+                            What does it mean for someone to have too much hope? What will happen to a city if it has too much hope? 
+                            In a city built upon this single concept, will it stand as strong as the fervor they show?"
                 show={modal1Show}
                 onHide={() => setModal1Show(false)}
          />
-         <ModalTwo
+         <ModalPop
+                title=      "Clockwork and Its Broken Time"
+                sub=        "The time which haunts the self."
+                content=    "Time for everyone moves only in a single direction. Toward the future and leaving behind the past, 
+                            sealing our previous actions in the bottle of history. However... is this truly the case? 
+                            Much like how an ant cannot percieve the three-dimentional plain, 
+                            how can mere humans say that time cannot go backward? Or maybe even... stop.
+                            The flow of time that differs from person to person. A flow of time that can devour other instances.
+                            The time that can consumes life of a person."
                 show={modal2Show}
                 onHide={() => setModal2Show(false)}
          />
 
-         <ModalThree
+         <ModalPop
+                title=      "The Eternity That Never Lasted"
+                sub=        "The bliss of ignorance."
+                content=    "Eternal Happiness. What a wonderful ring it has in the ears. In a world where [Eternity] is exists, 
+                            the people would venture towards their dreams and fulfill their deepest wishes. 
+                            What can stop the people now if time was never a concern? Frolicking and frolicking...
+                            Enjoying their neverending lives... The people never realized what they had lost."
                 show={modal3Show}
                 onHide={() => setModal3Show(false)}
          />
